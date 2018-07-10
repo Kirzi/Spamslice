82b8b7554985b9cff3dce6314f230d7e02a54d82
Kirzi <11909976+Kirzi@users.noreply.github.com>
2018-07-10 19:57:54 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Spamslice
 
-Flags possible spammers on Reddit.
+Flags usernames with less than x (30 by default) posts, since most spammers only have a few posts. This will result in mostly false positives. However, if you're checking for spammers and see a flagged username, consider checking the user's profile for spammy behavior.
+
+To install, copy the entire contents of Spamslice.js to a new userscript in Tampermonkey, and save.
+
+By default, it flags any user on Reddit. To limit it to specific subreddits, change `https://*.reddit.com/*` to https://*.reddit.com/r/<subreddit>/*. For multiple subreddits, use multiple `@match` lines.
+
+Credit to /u/Fortinbraz and [this post](https://www.reddit.com/r/TheseFuckingAccounts/comments/8gb6a8/metabookmarklet_to_help_identify_karma_farming/), since I copied a lot of the code from there.
---
+++ Spamslice.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Spamslice
+// @namespace    https://github.com/Kirzi/Spamslice
+// @version      1.0
+// @description  Flags possible spammers on Reddit
+// @author       Kirzi (blackaurora)
+// @match        https://*.reddit.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+function main() {
+
+    var a = {},
+        t = [];
+    $(".author").each(function() {
+        var e = $(this).text();
+        null == a[e] ? (a[e] = [this], t.push(e)) : a[e].push(this)
+    }), $.each(t, function(t) {
+        var e = this;
+        $.getJSON("https://www.reddit.com/user/" + this + ".json?limit=100", function(t) {
+            var postCount = t.data.dist;
+            $.each(a[e], function() {
+                var mod = $(this);
+                var threshold = 30; // can be between 1-100
+                if (postCount <= threshold) {
+                    // flags usernames with less than x posts (x = threshold)
+                    mod.wrap('<span style="font-weight: bold; background: rgba(0,0,0,.2); padding: 0 5px; border-radius: 4px;"></span>');
+                    mod = mod.parent();
+                };
+            })
+        })
+    })
+
+}
+
+window.setTimeout(main, 10);
+
+
+
+
